5c4dc636479f5b1add6107f59e419c8ad088e18b
Gleb Kalachev <megas781@gmail.com>
2019-03-16 15:12:24 +0300

    Инициализирована файловая структура проекта

--- FrontController.php
+++ FrontController.php
@@ -1,6 +1,27 @@
 <?php
 
 
-echo 'The file FrontController.php succeeded!';
+//echo 'The file FrontController.php succeeded! <br>';
+//
+//$rq = $_SERVER['REQUEST_URI'];
+//
+//echo 'Вас запрос: ' . $rq;
+
+
+    //FRONT CONTROLLER
+
+
+    //1. Общие настройки
+
+
+    //2. Подключение файлов системы
+
+
+    //3. Установка соединения с БД
+
+
+    //4. Вызов Router
+
+
 
 ?>
